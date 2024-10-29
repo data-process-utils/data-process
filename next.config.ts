c9552b559314bf44a042254c47d3887d06331813
GitHub Action <action@github.com>
2024-10-29 00:14:15 -0300

    ajustes para compilação da aplicação

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    basePath:"/json-finder",
   /* config options here */
-    // i18n:{
-    //     locales:["pt-BR","en-US"],
-    //     defaultLocale: "pt-BR",
-    //
-    // },
+    i18n:{
+        locales:["pt-BR","en-US"],
+        defaultLocale: "pt-BR",
+
+    },
     images: {
         remotePatterns: [
             {
